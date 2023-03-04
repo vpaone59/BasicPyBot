5ca36ff26fbed3ce4535547f42cb27028d2d6fd4
vinny <vpaone59@gmail.com>
2023-03-04 00:22:45 -0500

    git status Added dotenv import and load_env()

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ To run:
 """
 
 import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 import asyncio
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
